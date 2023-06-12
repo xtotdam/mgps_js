d2787f9d913636f6d1c3c38518070dad908ad73c
xtotdam <xtotdam@yandex.ru>
2023-06-12 19:13:09 +0300

    First actual commit

---
+++ js/found_items.js
@@ -0,0 +1,37 @@
+found_items = {
+    "Грибы &#x1F344;": {
+        "key": "mushroom",
+        "payload": [
+            {"name": "Белый",           "color": "#ffffff"},
+            {"name": "Подберезовик",    "color": "#873e23"},
+            {"name": "Подосиновик",     "color": "#cc0000"},
+            {"name": "Лисички",         "color": "#ffcc00"},
+            {"name": "Козелок",         "color": "#aaffaa"},
+            {"name": "Опята",           "color": "#aaaaaa"},
+            {"name": "Маслята",         "color": "#f5bc49"},
+            {"name": "Чернушка",        "color": "#000000"},
+            {"name": "Сыроежка",        "color": "#00ff00"},
+            {"name": "Шампиньон",       "color": "#888888"}
+        ],
+        "labelclass": "bg-secondary text-light"
+    },
+
+    "Ягоды &#x1F353;": {
+        "key": "plant",
+        "payload": [
+            {"name": "Земляника",       "color": "#F20739"},
+            {"name": "Черника",         "color": "#4D115C"},
+            {"name": "Малина",          "color": "#BE2561"},
+            {"name": "Брусника",        "color": "#ff0000"},
+            {"name": "Костяника",       "color": "#F21707"},
+            {"name": "Клюква",          "color": "#F20739"},
+            {"name": "Мох",             "color": "#298040"},
+            {"name": "Ствол",           "color": "#8f6728"}
+        ],
+        "btnclass": "bg-success text-light"
+    },
+
+    // Ориентиры
+}
+
+
